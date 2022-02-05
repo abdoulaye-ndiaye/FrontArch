864ce992c0615be42361708176db712a149411f3
Abdoulaye Ndiaye <wizlaye7@gmail.com>
2022-02-05 17:59:33 +0000

    a

--- src/app/views/changer-password/changer-password.component.ts
+++ src/app/views/changer-password/changer-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { MustMatch } from '../../services/_helpers/must-match.validator';
+
 
 @Component({
   selector: 'app-changer-password',
@@ -6,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangerPasswordComponent implements OnInit {
   inputText: string = 'changer-password';
-  constructor() { }
+  submitted = false;
+  changerPasswordForm: FormGroup;
+
+
+  constructor(
+    private authService: AuthService,
+    private formBulder: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.changerPasswordForm = this.formBulder.group({
+      password: ['', [Validators.required,Validators.minLength(6)]],
+      confirm: ['', Validators.required]
+    },
+    { validator: MustMatch('password', 'confirmPassword') })
+  }
+
+  get f() { return this.changerPasswordForm.controls; }
+
+  get value() {
+    return this.changerPasswordForm.controls;
+  }
+  onSubmit(){
+
   }
 
 }
