50887b3c6286fb83707b4f7c66a81a60dcecc942
Souaibou Ndiaye <ndiayevito04@gmail.com>
2022-02-08 00:07:13 +0000

    fix bug datatable

--- src/app/views/resp-form/liste-etudiants/liste-etudiants.component.ts
+++ src/app/views/resp-form/liste-etudiants/liste-etudiants.component.ts
@@ -1,44 +1,54 @@
-import { Component, OnInit, OnDestroy} from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-liste-etudiants',
-  templateUrl: './liste-etudiants.component.html'
+  templateUrl: './liste-etudiants.component.html',
 })
 export class ListeEtudiantsComponent implements OnInit {
   inputText: string = 'resp-form';
-  dtOptions: DataTables.Settings = {};
-  dtTrigger: Subject<any> = new Subject<any>();
 
   listeForm: FormGroup;
-  profil = sessionStorage.getItem("profil");
-  allEtudiants: any;
+  profil = sessionStorage.getItem('profil');
+  allEtudiantsSI: Array<any> = [];
+  allEtudiantsSR: Array<any> = [];
   submitted = false;
 
   constructor(
     private formBulder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService,
-  ) { }
-
-  
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-      
-    this.dtOptions = {
-      pagingType: 'full_numbers',
-      pageLength: 5,
-      lengthMenu: [5, 10, 25],
-      processing: true,
-      retrieve: true,
-    };
-    this.authService.listeEtudiant().subscribe(data => {
-      this.allEtudiants = data;
-      this.dtTrigger.next(data);
+    this.getEtudiants();
+  }
+
+  getEtudiants() {
+    this.authService.listeEtudiant().subscribe((data) => {
+      this.allEtudiantsSI = (data as any).filter(
+        (item: any) => item.classe === 'SI'
+      );
+      this.allEtudiantsSR = (data as any).filter(
+        (item: any) => item.classe === 'SR'
+      );
+      setTimeout(() => {
+        $('#etudiantsSI').DataTable({
+          pagingType: 'full_numbers',
+          pageLength: 5,
+          processing: true,
+          lengthMenu: [5, 10, 25],
+        });
+        $('#etudiantsSR').DataTable({
+          pagingType: 'full_numbers',
+          pageLength: 5,
+          processing: true,
+          lengthMenu: [5, 10, 25],
+        });
+      }, 1);
     });
   }
 
